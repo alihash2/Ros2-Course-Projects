c08a23a0285f86aca481327e7a328bbb60baf6c5
Hashir Ali <97245319+alihash2@users.noreply.github.com>
2026-08-09 19:19:51 +0500

    Restructure repo into ROS2 workspace packages

---
+++ ms01_cpp_foundations/build/CMakeFiles/mobile_robot_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mobile_robot_app.dir/apps/main.cpp.o"
+  "CMakeFiles/mobile_robot_app.dir/apps/main.cpp.o.d"
+  "mobile_robot_app"
+  "mobile_robot_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mobile_robot_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ms01_cpp_foundations/build/CMakeFiles/mobile_robot_lib.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mobile_robot_lib.dir/src/lidar_data.cpp.o"
+  "CMakeFiles/mobile_robot_lib.dir/src/lidar_data.cpp.o.d"
+  "CMakeFiles/mobile_robot_lib.dir/src/mobile_robot.cpp.o"
+  "CMakeFiles/mobile_robot_lib.dir/src/mobile_robot.cpp.o.d"
+  "CMakeFiles/mobile_robot_lib.dir/src/odometry.cpp.o"
+  "CMakeFiles/mobile_robot_lib.dir/src/odometry.cpp.o.d"
+  "CMakeFiles/mobile_robot_lib.dir/src/wheel_data.cpp.o"
+  "CMakeFiles/mobile_robot_lib.dir/src/wheel_data.cpp.o.d"
+  "libmobile_robot_lib.a"
+  "libmobile_robot_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mobile_robot_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
