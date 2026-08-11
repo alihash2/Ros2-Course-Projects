394a573c6697bbc5d89a07261a195093a13ba085
Hashir Ali <97245319+alihash2@users.noreply.github.com>
2026-08-11 12:29:56 +0500

    Issue 3 and 4, implementing pose subscriber and velocity feedback

--- ms02_mobile_robot_control/include/ms02_mobile_robot_control/goto_pose_node.hpp
+++ ms02_mobile_robot_control/include/ms02_mobile_robot_control/goto_pose_node.hpp
@@ -1,30 +1,30 @@
 #pragma once
-#include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <turtlesim/msg/pose.hpp>
+#include <rclcpp/rclcpp.hpp>                // Core ROS 2 library: Node base class, timers, logging, and execution spinning
+#include <geometry_msgs/msg/twist.hpp>      // Velocity message type: controls motion via linear.x and angular.z
+#include <turtlesim/msg/pose.hpp>          // Robot feedback message type: provides live position (x, y) and orientation (theta)
 
-class GoToPoseNode : public rclcpp::Node{
-    public:
-        GoToPoseNode(float target_x, float target_y, float target_theta);
+class GoToPoseNode : public rclcpp::Node {
+public:
+    GoToPoseNode(float target_x, float target_y, float target_theta);
 
-    private:
-        void poseCallback(const turtlesim::msg::pose::SharedPtr msg);
-        void controlLoop();
-        double normalizeAngle(double angle);
+private:
+    void poseCallback(const turtlesim::msg::Pose::SharedPtr msg);
+    void controlLoop();
+    double normalizeAngle(double angle);
 
-        rclcpp::Subscription<turtlesim::msg::pose>::SharedPtr pose_sub_;
-        rclcpp::Publisher<geometry_msgs/msg::Twist>::SharedPtr cmd_vel_pub_;
-        rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr pose_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
-        turtlesim::msg::pose current_pose_;
-        bool pose_recieved_{false};
+    turtlesim::msg::Pose current_pose_;
+    bool pose_received_{false};
 
-        double target_x_;
-        double target_y_;
-        double target_thetha_;
+    double target_x_;
+    double target_y_;
+    double target_theta_;
 
-        const double K_linear_{1.5};
-        const double K_angular_{4.0};
-        const double distance_tolerance_{0.05};
-        const double angle_tolerance_{0.02};
+    const double K_linear_{1.5};
+    const double K_angular_{4.0};
+    const double distance_tolerance_{0.05};
+    const double angle_tolerance_{0.02};
 };
